a4b8e980eb14e90aaf1a7d8816803e4a3b25fb23
therealmtam <max.c.tam@gmail.com>
2022-12-17 08:33:31 -0800

    added jest tutorial

---
+++ jest/jestNotes.js
@@ -0,0 +1,59 @@
+/*
+- basic tutorial setup if no config file for jest (jest.config.js)
+  - https://jestjs.io/docs/getting-started
+
+  - all you need is a file that meets this criteria in naming:
+    - testMatch: */__tests__/* /*.[jt]s?(x), * * /?(*.)+(spec|test).[tj]s?(x)
+
+    - so file name neests to be XX.test.ts/js or XX.spec.ts/js
+
+    - the contents of the file can be as simple as:
+
+      function sum(a, b) {
+        return a + b;
+      }
+      module.exports = sum;
+
+      // const sum = require('./sum');
+
+      test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+      });
+
+  - then add a script as
+    "test": "jest"
+
+  - then jest will use its out of the box config to find those files and tests and run
+
+
+-----------
+- more advanced setup with a jest.config file to control the config of the tests
+
+
+initial setup
+---------------
+- https://jestjs.io/docs/getting-started#running-from-command-line
+
+- first you add jest globally to your npm packages
+  > npm install jest --global
+
+- then you can use the comamand
+  > jest --init
+
+    - there is [jest.config__tsversion.ts] which is the output after you run it and say you want a ts version of the config and running the app for node
+
+    - there is [jest.config.js] which is the output after you run it and say you want a js version of the config running the tests in v8 (node)
+
+      - this is the one the [advanced] tests will use since using the ts version of the config requires additional packages
+
+        - TODO - a tutorial on what additional packages are needed for ts to work
+          - Note: that if the test files are in ts and use ts stuff like import or types, you need to do additional stuff for that as well. BUT if the test file is just named as ts but does not contain ts related terms, then you can run it with js config without any issues
+
+          - https://jestjs.io/docs/getting-started#using-typescript
+
+- after the config file is built, (not the TS version of the config which requires more research ^), you can just do the same as the basic config and run
+
+  > yarn test
+
+
+*/
